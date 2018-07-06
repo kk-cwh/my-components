50fc578e68b296b466a9dfa59e248bb2b9ecd862
jaak <zhangykvip@126.com>
2018-07-06 13:40:39 +0800

    :art: :sparkles: add qrcode VueParticles and VueCanvasNest :fire:

---
+++ src/components/VueParticles/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+   <canvas class="particles-background" ref="vue_particles_canvas">
+   </canvas>
+</div>
+
+</template>
+<script>
+
+import Particles from 'particlesjs'
+export default {
+  name: 'vueCanvasNest',
+  props: {
+    config: {
+      type: Object,
+      default () {
+        return {
+          maxParticles: 100,
+          sizeVariations: 3,
+          speed: 0.5,
+          color: '#000000',
+          minDistance: 120,
+          connectParticles: false,
+          responsive: []
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      cn: ''
+    }
+  },
+  mounted () {
+    Particles.init({selector: '.particles-background', ...this.config})
+  },
+  beforeDestroy () {
+    Particles.destroy()
+  }
+}
+</script>
+<style scoped>
+.particles-background {
+  position: absolute;
+  display: block;
+  top: 0;
+  left: 0;
+  z-index: -2;
+}
+</style>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="hello">
+    <VueParticles :config="config"></VueParticles>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank">
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">
+          Forum
+        </a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank">
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">
+          vue-router
+        </a>
+      </li>
+      <li>
+        <router-link to="/canvas">canvas</router-link>
+
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import VueParticles from '@/components/VueParticles'
+export default {
+  components: {
+    VueParticles
+  },
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      config: {
+        color: '#66ee99',
+        speed: 0.5,
+        sizeVariations: 5,
+        connectParticles: true,
+        minDistance: 100
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  z-index: 1;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
